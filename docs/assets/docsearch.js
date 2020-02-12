8ccde767c07315277d4719bb06918d7f2134f111
Bill Fienberg <billfienberg@gmail.com>
2020-02-12 11:00:08 -0600

    Remove HTML comment

---
+++ docs/assets/docsearch.js
@@ -0,0 +1,15 @@
+var appId = "ZVM90TV1LT"
+var apiKey = "bb8084db89e83e08f6d0df0fbfda0ffa"
+var indexName = "docsearch"
+var inputSelector = "#docsearch-input"
+
+// Set debug to true if you want to inspect the dropdown
+var debug = false
+
+docsearch({
+  appId: appId,
+  apiKey: apiKey,
+  indexName: indexName,
+  inputSelector: inputSelector,
+  debug: debug,
+})
